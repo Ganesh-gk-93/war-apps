25995213c71e53a4b988f65fdbbe76b71611b9ad
ganesh-gk-93 <ganeshgk93@gmail.com>
2026-04-19 18:40:04 +0530

    upda

--- build.sh
+++ build.sh
@@ -3,24 +3,18 @@ set -e
 
 export MINIKUBE_HOME=/var/lib/jenkins/.minikube
 export KUBECONFIG=/var/lib/jenkins/.kube/config
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.49.2:2376"
+export DOCKER_CERT_PATH="/var/lib/jenkins/.minikube/certs"
 
 K8S_APPS=("anniversary" "celebration" "devops-app" "ganesh-harini" "tom-and-jerry")
 
-echo "=== Using VM Docker daemon ==="
-eval $(minikube docker-env --unset)
-
-echo "=== Building Docker images ==="
+echo "=== Building images directly inside Minikube Docker ==="
 docker build --build-arg APP_NAME=anniversary   -t anniversary:latest   -f docker/Dockerfile .
 docker build --build-arg APP_NAME=celebration   -t celebration:latest   -f docker/Dockerfile .
 docker build --build-arg APP_NAME=devops-app    -t devops-app:latest    -f docker/Dockerfile .
 docker build --build-arg APP_NAME=ganesh_harini -t ganesh-harini:latest -f docker/Dockerfile .
 docker build --build-arg APP_NAME=tom_and_jerry -t tom-and-jerry:latest -f docker/Dockerfile .
-
-echo "=== Loading images into Minikube ==="
-for APP in "${K8S_APPS[@]}"; do
-  echo "--- Loading $APP ---"
-  docker save $APP:latest | minikube ssh --native-ssh=false -- docker load
-done
 
 echo "=== Deploying to Kubernetes ==="
 kubectl config use-context minikube
